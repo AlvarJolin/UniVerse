26fc556b02f25405f6a7763eefebf6e5753df5ae
AlvarJolin <102738477+AlvarJolin@users.noreply.github.com>
2022-06-06 10:40:42 +0200

    Update Uniproject.js

--- Uniproject.js
+++ Uniproject.js
@@ -20,7 +20,7 @@ function search_subject() {
 	let x = document.getElementsByClassName('subject');
 
 	for (i = 0; i < x.length; i++) {
-		if (!x[i].innerHTML.toLowerCase().includes(input)) {
+		if (!x[i].cells[0].innerHTML.toLowerCase().includes(input)) {
 			x[i].style.display = "none";
 		} else {
 			x[i].style.display = "table-row";
